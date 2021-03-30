b6aa7adee0a918437de7636e746f702d0b6a0fb6
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-03-30 08:48:58 -0400

    latest add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-calculator';
+  n: number = 0;
+  m: number = 0;
+  ans: number = 0;
+  x: number = 0;
+  y: number = 0;
+  ans2: number = 0;
+  a: number = 0;
+  b: number = 0;
+  ans3: number = 0;
+  j: number = 0;
+  k: number = 0;
+  ans4: number = 0;
+  
+  add(): void {
+    this.ans = this.n + this.m;
+  }
+  subtract(): void {
+    this.ans2 = this.x - this.y;
+  }
+  multiply(): void {
+    this.ans3 = this.a * this.b;
+  }
+  divide(): void {
+    this.ans4 = this.j / this.k;
+  }
 }
+
+
